d27697cf56bbc93c86978b2e9cabe0eaf671c280
sdsmk <34293493+sdsmk@users.noreply.github.com>
2021-12-10 18:24:52 +0530

    Update and rename DataStreamDemo.java to Assignment5.java

--- Assignment5.java
+++ Assignment5.java
@@ -5,7 +5,7 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-public class DataStreamDemo {
+public class Assignment5 {
 
   public static void main(String[] args){
       
